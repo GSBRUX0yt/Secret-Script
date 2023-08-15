11d783f996c39b40cbcf7498faaa22080afa9015
GSBRUX0yt <142352751+GSBRUX0yt@users.noreply.github.com>
2023-08-15 19:01:23 -0300

    Add files via upload

--- Secrets.lua
+++ Secrets.lua
@@ -1,22 +1,27 @@
---Librery Secret Hub
+--Library Secret Hub
+
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+
 local Window = OrionLib:MakeWindow({Name = "Secret Hub", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+
 local PlayerTab = Window:MakeTab({
 	Name = "Player",
 	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false
 })
+
 local Section = PlayerTab:AddSection({
-	Name = "Movimentos"
+	Name = "Movement"
 })
+
 PlayerTab:AddSlider({
-	Name = "Velocidade",
+	Name = "WalkSpeed",
 	Min = 16,
 	Max = 500,
 	Default = 5,
 	Color = Color3.fromRGB(255,255,255),
 	Increment = 1,
-	ValueName = "Speed",
+	ValueName = "WS",
 	Callback = function(Value)
         game.Players.LocalPlayer.Character.Humanoid.JumpPower = Value
 	end    
