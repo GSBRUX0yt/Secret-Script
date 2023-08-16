1094f46136efc5ff74d3c1a1653645f1f84d370e
GSBRUX0yt <142352751+GSBRUX0yt@users.noreply.github.com>
2023-08-16 06:23:45 -0300

    Add files via upload

--- Secrets Hub.lua
+++ Secrets Hub.lua
@@ -39,3 +39,14 @@ local Window = Rayfield:CreateWindow({
       game.Players.LocalPlayer.Character:SetAttribute("SpeedMultiplier", Value)
     end,
  })
+ local Slider = PlayerTab:CreateSlider({
+   Name = "Dash length",
+   Range = {10, 1000},
+   Increment = 1,
+   Suffix = "Length",
+   CurrentValue = 10,
+   Flag = "Slider2", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Callback = function(Value)
+    game.Players.LocalPlayer.Character:SetAttribute("DashLength", Value)
+   end,
+})
